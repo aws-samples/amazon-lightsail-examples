b7ab2ec7c9fbd4325e723df6e05dd4d14716da68
developer-1 <hnsoni@amazon.co.uk>
2023-11-14 17:07:56 +0000

    added comments

--- lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
+++ lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
@@ -1,9 +1,9 @@
 <script>
+    #start audio service
     net start audiosrv
-
+    #set reg key to start narrator at logon
     reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Accessibility" /t REG_SZ /v Configuration /d narrator /f
-
+    #start narrator
     narrator
 </script>
-
 <persist>true</persist>
