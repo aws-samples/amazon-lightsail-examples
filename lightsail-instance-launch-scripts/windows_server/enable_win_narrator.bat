fd02dea3f787108babe96817523cde75bf6eeb8d
developer-1 <hnsoni@amazon.co.uk>
2023-11-14 09:50:34 +0000

    added md file and removed the comments from the script.

--- lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
+++ lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
@@ -1,10 +1,9 @@
-# The code below can be used enable accessibility for a visually impaired feature of narrator and runs it always on logon.
-# This is based on a request on re:post https://repost.aws/questions/QUjkaxc6x0QHqzdwr6RxxRWQ/server-accessibility-for-a-visually-impaired#ANs3zh89yiR7S4RRZPNVh14g
-# Note that this is a simple script that can be used as a part of the launch config.
-# Note: To take advantage of the sound on the lightsail instance a native RDP client which can playback sound from remote machine should be used to connect.
 <script>
     net start audiosrv
+
     reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Accessibility" /t REG_SZ /v Configuration /d narrator /f
+
     narrator
 </script>
+
 <persist>true</persist>
---
+++ lightsail-instance-launch-scripts/windows_server/enable_win_narrator.md
@@ -0,0 +1,7 @@
+# About enable win narrator script
+
+The script can be used to enable accessibility features on window for a visually impaired person and start the audio service, run narrator and is set to always runs on logon.
+This is based on a request on re:post https://repost.aws/questions/QUjkaxc6x0QHqzdwr6RxxRWQ/server-accessibility-for-a-visually-impaired#ANs3zh89yiR7S4RRZPNVh14g
+## Note 
+ 1. This is a simple script that can be used as a part of the launch config. 
+ 2. To take advantage of the sound on the lightsail instance a native RDP client which can playback sound from remote machine should be used to connect.
