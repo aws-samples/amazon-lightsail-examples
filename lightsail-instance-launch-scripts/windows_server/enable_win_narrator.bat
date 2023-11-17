5856e70247071474b83a57309a570ab8d2dbaf93
developer-1 <hnsoni@amazon.co.uk>
2023-11-17 23:41:34 +0000

    completely changed the approach to remove persist = true

--- lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
+++ lightsail-instance-launch-scripts/windows_server/enable_win_narrator.bat
@@ -1,10 +1,10 @@
 <script>
-    #start audio service
-    net start audiosrv
-    #set reg key to start narrator at logon
-    reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Accessibility" /t REG_SZ /v Configuration /d narrator /f
-    #start narrator
-    narrator
+    #create a new startup script statup_script.bat and  add start audio service to the script
+    echo net start audiosrv > c:\startup_script.bat
+    #add start narrator
+    echo narrator >> c:\startup_script.bat
+    #add exit cmd window
+    echo exit >> c:\startup_script.bat
+    #create shortcut to startup_script.bat in AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ to run the script everytime
+    powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup_script.lnk');$s.TargetPath='c:\startup_script.bat';$s.Save()"
 </script>
-#To run the user data scripts every time you reboot or start the instance
-<persist>true</persist>
